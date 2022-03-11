4c5a58870b6ec513a2e09aa3ae214a6a29e0ef52
Priyanka Rudrawar <priyarudrawar718@gmail.com>
2022-03-10 23:16:59 -0500

    34

---
+++ Priyanka Rudrawar/Backend/SQL/DLithe_BC_NFS_T_Task34_SQLSERVER/SQL2.sql
@@ -0,0 +1,71 @@
+use DLithe;
+
+
+CREATE FUNCTION TE(@a INT)
+returns INT
+AS
+BEGIN
+return @a+@a
+END
+
+SELECT dbo.TE(10) as 'Result'
+
+--=======================================
+--CREATE  TABLE
+CREATE TABLE EMP
+(
+ID INT,
+NAME VARCHAR(25),
+DOB 
+
+
+
+--========================================
+--CRETATE FUNCTION FOR THE DOB COLUMN IN EMP
+create function calcage(@dob date)
+Returns int
+as
+begin
+	declare @age int
+	set @age=DATEDIFF(year,@dob,getdate())
+	return @age
+end
+
+select dbo.calcage('12/12/02') as Age;
+
+
+SELECT ID,NAME,DOB,dbo.calcage(DOB) AS AGE FROM EMP;
+
+
+
+
+----===================================
+CREATE FUNCTION INL_TBL(@ID INT)
+RETURNS TABLE
+AS
+RETURN(SELECT * FROM EMP WHERE EID=@ID)
+
+SELECT * FROM INL_TBL(6)
+
+SELECT * FROM DLITHE_TRAINEES
+
+
+CREATE FUNCTION DLITHE_TRAINEES_FUN(@DIT VARCHAR(25))
+RETURNS TABLE
+AS
+RETURN(SELECT * FROM DLITHE_TRAINEES WHERE TDEPT=@DIT)
+
+
+SELECT * FROM DLITHE_TRAINEES_FUN('DITDOTNET')
+
+
+----===============================================
+--==MULTI STATEMENT 
+CREATE FUNCTION MULTI_STMT()
+RETURNS @TABLE TABLE(ID INT, NAME VARCHAR(25), DEPARTMENT VARCHAR(25))
+AS
+BEGIN
+INSERT INTO @TABLE
+SELECT TID,TNAME,TDEPT FROM DLITHE_TRAINEES WHERE TADDRESS='BANGALORE';
+RETURN
+END
